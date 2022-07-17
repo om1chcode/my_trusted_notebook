8ce471048cab1612f0223b05d7ce9258a1a8404b
om1chcode <om1chcode@gmail.com>
2022-07-18 00:36:11 +0300

    MyTrustedNotebook :: trying basic jetpack compose tutorial

--- MyTrustedNotebook/app/src/main/java/org/om1chcode/mytrustednotebook/ActivityMyTrustedNotebook.kt
+++ MyTrustedNotebook/app/src/main/java/org/om1chcode/mytrustednotebook/ActivityMyTrustedNotebook.kt
@@ -1,44 +1,144 @@
 package org.om1chcode.mytrustednotebook
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.material.Surface
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
-
+import androidx.compose.ui.unit.dp
 import org.om1chcode.mytrustednotebook.ui.theme.MyTrustedNotebookTheme
 
+// ---------------------------------------------------------------------------------------------------------------------
 class MainActivity : ComponentActivity()
 {
 	override fun onCreate(savedInstanceState : Bundle?)
 	{
 		super.onCreate(savedInstanceState)
 		setContent {
-			MyTrustedNotebookTheme { // A surface container using the 'background' color from the theme
-				Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-					Greeting("some note")
-				}
+			MyTrustedNotebookTheme {
+				//MessageCard(Message("Here", "It is and may be"))
+				Conversation( listOf(
+					Message( "Here", "There\nThere" ),
+					Message( "Left", "Right\nWrong" ),
+					Message( "Wrong", "True\n\nThe" ),
+					Message( "Little", "Where\nWhe" ),
+					Message( "One", "Three\n2\n3" ),
+					Message( "Two", "Four\n2\n3\n4" ),
+				) )
 			}
 		}
 	}
 }
 
+// ---------------------------------------------------------------------------------------------------------------------
 @Composable
-fun Greeting(name : String)
-{
-	Text(text = "Here it may be $name!")
+fun Conversation(messages: List<Message>) {
+	LazyColumn {
+		items(messages) { message ->
+			MessageCard(message)
+		}
+	}
 }
 
-@Preview(showBackground = true)
+// ---------------------------------------------------------------------------------------------------------------------
+data class Message(val author: String, val body: String)
+
+// ---------------------------------------------------------------------------------------------------------------------
 @Composable
-fun DefaultPreview()
-{
+fun MessageCard(msg: Message) {
+	// Add padding around our message
+
+	Row( modifier = Modifier.padding(all = 8.dp) ) {
+		Image(
+			painter = painterResource(R.drawable.ic_launcher_background),
+			contentDescription = "some random square",
+			modifier = Modifier				// Set image size to 40 dp
+				.size(40.dp)				// Clip image to be shaped as a circle
+				.clip(CircleShape)
+				.border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+		)
+
+		// Add a horizontal space between the image and the column
+		Spacer(modifier = Modifier.width(8.dp))
+
+		// We keep track if the message is expanded or not in this
+		// variable
+		var isExpanded by remember { mutableStateOf(false) }
+
+		val surfaceColor by animateColorAsState(
+			if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+		)
+
+		Column(modifier = Modifier.clickable { isExpanded = !isExpanded }){
+			Text(
+				text = msg.author,
+				color = MaterialTheme.colors.secondaryVariant,
+				style = MaterialTheme.typography.subtitle2
+			)
+			// Add a vertical space between the author and message texts
+			Spacer(modifier = Modifier.height(4.dp))
+
+			Surface(
+				shape = MaterialTheme.shapes.medium,
+				elevation = 1.dp,
+				// surfaceColor color will be changing gradually from primary to surface
+				color = surfaceColor,
+				// animateContentSize will change the Surface size gradually
+				modifier = Modifier
+					.animateContentSize()
+					.padding(1.dp)
+
+			) {
+				Text(
+					text = msg.body,
+					modifier = Modifier.padding(all = 4.dp),
+					// If the message is expanded, we display all its content
+					// otherwise we only display the first line
+					maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+					style = MaterialTheme.typography.body2
+				)
+			}
+		}
+	}
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+@Preview( name ="Light Mode" )
+@Preview(
+	uiMode = Configuration.UI_MODE_NIGHT_YES,
+	showBackground = true,
+	name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
 	MyTrustedNotebookTheme {
-		Greeting("Android")
+		Conversation( listOf(
+			Message( "Here", "There\nThere" ),
+			Message( "Left", "Right\nWrong" ),
+			Message( "Wrong", "True\n\nThe" ),
+			Message( "Little", "Where\nWhe" ),
+			Message( "One", "Three\n2\n3" ),
+			Message( "Two", "Four\n2\n3\n4" ),
+		) )
 	}
 }
